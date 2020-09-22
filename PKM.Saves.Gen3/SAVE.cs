b018494712c675262d54eef2c1a0d7b9fe0ddbb6
JamPaul-Laptop <mantz@LAPTOP-06D32MGO>
2020-09-22 14:57:09 +0300

    Added test WinForm Project

--- PKM.Saves.Gen3/SAVE.cs
+++ PKM.Saves.Gen3/SAVE.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/* Gen3 Save Class
+ * By JamPaul97
+ * Version : 0.0.1
+ * Attribution-NonCommercial-NoDerivs 3.0 Unported (CC BY-NC-ND 3.0)
+ * https://github.com/JamPaul97
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +14,6 @@ namespace PKM.Saves.Gen3
 {
     public class Save
     {
+        
     }
 }
